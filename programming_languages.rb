170f5875acdb320a57d97566d5dbf5d129c15a59
heather sterman <heather.sterman531@gmail.com>
2018-10-23 00:39:25 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,6 @@
 def reformat_languages(languages)
-  new_hash = {:ruby => {:type => "interpreted", :style => [:oo]}}
+  new_hash = {:ruby => {:type => "interpreted", :style => [:oo]},
+  :javascript => {:type => "interpreted", :style => [:oo, :functional]}
+    
+  }
 end
